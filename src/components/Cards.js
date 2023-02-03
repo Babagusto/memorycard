5315bfff14e311fa04da19b4f420eb9a330c98aa
Babagusto <coleggibson@gmail.com>
2023-02-02 19:45:35 -0600

    Added images and imported to Cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+    Gameboy,
+    Nes,
+    Gamecube,
+    Nintendods,
+    Ps5, 
+    Sega, 
+    Switch, 
+    Xbox, 
+    Playstation
+} from '../../src/images'
+
+
 
 const Cards = () => {
-    return(
-    <div id="cardContainer">Howdy
 
+
+    return(
+    <div id="cardContainer">
+        <div id="gameboy" className="card">
+            <img src={Gameboy}></img>
+        </div>
     </div>
     )
 }
